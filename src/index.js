1c3070a57671b08234b2d52b2155dc821a165b7c
vsergiu <vsergiu93@gmail.com>
2018-09-25 23:51:52 +0300

    Export from index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const GAxis = require('./GAxis')
+const GPlot = require('./GPlot')
+const GPoint = require('./GPoint')
+const GLayer = require('./GLayer')
+const GAxisLabel = require('./GAxisLabel')
+const GHistogram = require('./GHistogram')
+const GTitle = require('./GTitle')
+
+module.exports = {
+    GAxis,
+    GPlot,
+    GPoint,
+    GLayer,
+    GAxisLabel,
+    GHistogram,
+    GTitle
+}
